ce5f93c88c39032a4fb2b075fca9dfdcadfd2c7c
Kris3579 <kristianesvelt@hotmail.com>
2018-07-24 15:12:09 -0700

    cleanup, broke up code into smaller functions

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var imgArray = [];
-var totalVotes = 0;
 var noRepeat = [];
 
-// var vote = document.getElementById('button');
+var totalVotes = 0;
+
+var clicked1 = document.getElementById('button1');
+var clicked2 = document.getElementById('button2');
+var clicked3 = document.getElementById('button3');
+
+clicked1.addEventListener('click', picked1);
+clicked2.addEventListener('click', picked2);
+clicked3.addEventListener('click', picked3);
+
+var Img1 = document.createElement('img');
+var Img2 = document.createElement('img');
+var Img3 = document.createElement('img');
+
+var random1;
+var random2;
+var random3;
 
 function Img(src) {
   this.src = src;
@@ -40,23 +55,6 @@ function imgRandom() {
   return imgArray[Math.floor(Math.random() * imgArray.length)];
 }
 
-var Img1 = document.createElement('img');
-var Img2 = document.createElement('img');
-var Img3 = document.createElement('img');
-var random1;
-var random2;
-var random3;
-// var noRepeat1;
-// var noRepeat2;
-// var noRepeat3;
-
-var clicked1 = document.getElementById('button1');
-var clicked2 = document.getElementById('button2');
-var clicked3 = document.getElementById('button3');
-
-clicked1.addEventListener('click', picked1);
-clicked2.addEventListener('click', picked2);
-clicked3.addEventListener('click', picked3);
 
 function picked1() {
   random1.clicks++;
@@ -64,11 +62,13 @@ function picked1() {
   renderImgs();
 }
 
+
 function picked2() {
   random2.clicks++;
   totalVotes++;
   renderImgs();
 }
+
 
 function picked3() {
   random3.clicks++;
@@ -76,44 +76,60 @@ function picked3() {
   renderImgs();
 }
 
-function onLoad() {
+
+function renderTotals() {
+  random1.rendered++;
+  random2.rendered++;
+  random3.rendered++;
+}
+
+
+function removeImgs() {
+  document.getElementById('1').removeChild(Img1);
+  document.getElementById('2').removeChild(Img2);
+  document.getElementById('3').removeChild(Img3);
+}
+
+
+function reRollImgs() {
   random1 = imgRandom();
   random2 = imgRandom();
   random3 = imgRandom();
+}
+
+
+function pushRandoms() {
+  noRepeat.push(random1);
+  noRepeat.push(random2);
+  noRepeat.push(random3);
+}
+
+
+function onLoad() {
+  reRollImgs();
+
   while (random1 === random2 || random1 === random3 || random2 === random3) {
     random2 = imgRandom();
     random3 = imgRandom();
   }
-  noRepeat.push(random1);
-  noRepeat.push(random2);
-  noRepeat.push(random3);
-  console.log('onLoad noRepeat', noRepeat);
+
+  pushRandoms();
   Img1.src = random1.src;
   document.getElementById('1').appendChild(Img1);
   Img2.src = random2.src;
   document.getElementById('2').appendChild(Img2);
   Img3.src = random3.src;
   document.getElementById('3').appendChild(Img3);
+  renderTotals();
 }
 
 
 function renderImgs() {
-  document.getElementById('1').removeChild(Img1);
-  document.getElementById('2').removeChild(Img2);
-  document.getElementById('3').removeChild(Img3);
-  random1 = imgRandom();
-  random2 = imgRandom();
-  random3 = imgRandom();
-  // console.log(random1);
-  // console.log(random2);
-  // console.log(random3);
+  removeImgs();
+  reRollImgs();
 
   while (random1 === random2 || random1 === random3 || random2 === random3 || noRepeat.includes(random1) || noRepeat.includes(random2) || noRepeat.includes(random3)) {
-    console.log('catch dupe', noRepeat);
-    random1 = imgRandom();
-    random2 = imgRandom();
-    random3 = imgRandom();
-    console.log('noRepeat', noRepeat);
+    reRollImgs();
   }
 
   Img1.src = random1.src;
@@ -122,21 +138,9 @@ function renderImgs() {
   document.getElementById('2').appendChild(Img2);
   Img3.src = random3.src;
   document.getElementById('3').appendChild(Img3);
-
-  // noRepeat.pop();
-  // noRepeat.pop();
-  // noRepeat.pop();
+  renderTotals();
   noRepeat = [];
-  noRepeat.push(random1);
-  noRepeat.push(random2);
-  noRepeat.push(random3);
-  console.log('reasigned noRepeat', noRepeat);
-  // console.log(random1);
-  // console.log(random2);
-  // console.log(random3);
-  // noRepeat1 = random1;
-  // noRepeat2 = random2;
-  // noRepeat3 = random3;
+  pushRandoms();
 }
 
 onLoad();
