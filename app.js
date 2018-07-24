17c8f564ee1281f6758fe7982aded2afc5683e93
Kris3579 <kristianesvelt@hotmail.com>
2018-07-24 14:53:09 -0700

    got no dupes from precious set working

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var imgArray = [];
+var totalVotes = 0;
+var noRepeat = [];
 
 // var vote = document.getElementById('button');
 
@@ -41,9 +43,9 @@ function imgRandom() {
 var Img1 = document.createElement('img');
 var Img2 = document.createElement('img');
 var Img3 = document.createElement('img');
-var random1 = imgRandom();
-var random2 = imgRandom();
-var random3 = imgRandom();
+var random1;
+var random2;
+var random3;
 // var noRepeat1;
 // var noRepeat2;
 // var noRepeat3;
@@ -58,24 +60,34 @@ clicked3.addEventListener('click', picked3);
 
 function picked1() {
   random1.clicks++;
+  totalVotes++;
   renderImgs();
 }
 
 function picked2() {
   random2.clicks++;
+  totalVotes++;
   renderImgs();
 }
 
 function picked3() {
   random3.clicks++;
+  totalVotes++;
   renderImgs();
 }
 
-function start() {
+function onLoad() {
+  random1 = imgRandom();
+  random2 = imgRandom();
+  random3 = imgRandom();
   while (random1 === random2 || random1 === random3 || random2 === random3) {
     random2 = imgRandom();
     random3 = imgRandom();
   }
+  noRepeat.push(random1);
+  noRepeat.push(random2);
+  noRepeat.push(random3);
+  console.log('onLoad noRepeat', noRepeat);
   Img1.src = random1.src;
   document.getElementById('1').appendChild(Img1);
   Img2.src = random2.src;
@@ -83,6 +95,7 @@ function start() {
   Img3.src = random3.src;
   document.getElementById('3').appendChild(Img3);
 }
+
 
 function renderImgs() {
   document.getElementById('1').removeChild(Img1);
@@ -91,24 +104,39 @@ function renderImgs() {
   random1 = imgRandom();
   random2 = imgRandom();
   random3 = imgRandom();
-  while (random1 === random2 || random1 === random3 || random2 === random3) {
+  // console.log(random1);
+  // console.log(random2);
+  // console.log(random3);
+
+  while (random1 === random2 || random1 === random3 || random2 === random3 || noRepeat.includes(random1) || noRepeat.includes(random2) || noRepeat.includes(random3)) {
+    console.log('catch dupe', noRepeat);
+    random1 = imgRandom();
     random2 = imgRandom();
     random3 = imgRandom();
-    // while (noRepeat1 === random1, random2, random3 || noRepeat2 === random1, random2, random3 || noRepeat3 === random1, random2, random3) {
-    //   random1 = imgRandom();
-    //   random2 = imgRandom();
-    //   random3 = imgRandom();
-    // }
+    console.log('noRepeat', noRepeat);
   }
+
   Img1.src = random1.src;
   document.getElementById('1').appendChild(Img1);
   Img2.src = random2.src;
   document.getElementById('2').appendChild(Img2);
   Img3.src = random3.src;
   document.getElementById('3').appendChild(Img3);
+
+  // noRepeat.pop();
+  // noRepeat.pop();
+  // noRepeat.pop();
+  noRepeat = [];
+  noRepeat.push(random1);
+  noRepeat.push(random2);
+  noRepeat.push(random3);
+  console.log('reasigned noRepeat', noRepeat);
+  // console.log(random1);
+  // console.log(random2);
+  // console.log(random3);
   // noRepeat1 = random1;
   // noRepeat2 = random2;
   // noRepeat3 = random3;
 }
 
-start();
+onLoad();
